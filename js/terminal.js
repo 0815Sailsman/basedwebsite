2ed77a44a72e0b3a3ea8b1c7658bc024363dad7a
0815Sailsman <sailsman@tutanota.com>
2023-04-24 23:22:14 +0200

    Can now type in terminal

---
+++ js/terminal.js
@@ -0,0 +1,42 @@
+window.addEventListener('load', wait, false);
+
+function wait() {
+var observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    if (mutation.target.classList.contains('being-typed-out')) {
+	    console.log("a");
+      selectInput();
+    }
+  });
+});
+
+observer.observe(document.getElementById('last-to-be-typed'), {
+    attributes: true,
+    attributeFilter: ['class']
+});
+}
+
+function selectInput () {
+  var input = document.getElementById('terminal-input');
+  input.focus();
+  input.select();
+	input.addEventListener("input", displayInput, false);
+	input.addEventListener("keyup", function(e) {
+	    if (e.key == "Enter") {
+		    enterKeyPressed();
+		}
+	       });
+};
+
+function displayInput() {
+	var display = document.getElementById("terminal-line");
+  var input = document.getElementById('terminal-input');
+	display.innerHTML = input.value;
+}
+
+function enterKeyPressed() {
+		var display = document.getElementById("terminal-line");
+  var input = document.getElementById('terminal-input');
+	input.value="";
+	display.innerHTML = "";
+}
