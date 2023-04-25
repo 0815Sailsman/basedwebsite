7f9884c8208203c29dace0f376bfd21b93040a93
0815Sailsman <sailsman@tutanota.com>
2023-04-25 13:25:38 +0200

    More imgs

--- js/random-img.js
+++ js/random-img.js
@@ -4,7 +4,8 @@ function randomImgMain() {
 	var myImage = document.getElementById("topimg");
 	const possible = [
 		"blob.gif", "pepeJam.webp", "pepeD.webp", "catJAM.webp", "bongoTap.webp", "blobDance.webp", "nodders.webp", "dance.webp",
-		"pepoDance.webp", "pepeDs.webp", "monkaSteer.webp", "partyParrot.webp", "pepeJamJam.webp", "PEPEDS.webp", "ratJAM.webp"
+		"pepoDance.webp", "pepeDs.webp", "monkaSteer.webp", "partyParrot.webp", "pepeJamJam.webp", "PEPEDS.webp", "ratJAM.webp",
+		"PagBounce.webp", "hypeE.webp", "MODS.webp"
 	];
 	myImage.src = "imgs/" + possible[getRandomInt(possible.length)];
 };
